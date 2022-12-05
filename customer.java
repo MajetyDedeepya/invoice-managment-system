6446da7f1e563ccf02a09b58dc5569306e06f6d1
MajetyDedeepya <78795096+MajetyDedeepya@users.noreply.github.com>
2022-12-05 11:20:34 +0530

    Create customer.java

---
+++ customer.java
@@ -0,0 +1,44 @@
+package invoicebookingmanagment;
+
+public class customer {
+	
+	private int customerId;
+	private String name;
+	private String address;
+	private String phoneNumber;
+	public customer(int customerId,String name, String address, String phoneNumber) {
+		
+		
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.customerId =customerId;
+		
+		
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
